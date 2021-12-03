01273f7a47865a016f6553d03c08c043e32fa012
Pavel Semyonov <pavel@ndb.technology>
2021-12-03 16:13:32 +0200

    purchase auction styled

--- src/pages/purchase/auction/Auction.jsx
+++ src/pages/purchase/auction/Auction.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react"
-import styled from "styled-components";
-import "./style.scss";
+import Slider from 'rc-slider';
 import Header from "./../../../components/common/header";
 import { getSecTomorrow, numberWithLength } from "./../../../utilities/number"
-
+import { AuctionLeft, AuctionRight, Title, TableContainer, SliderContainer, TimeBar, TotalPrice, BuyButton } from "./styledComponents";
+import "./style.scss";
 
 const Auction = () => {
-    const [tabIndex, setTabIndex] = useState(0)
     const duration = 86400
     const [curTime, setCurTime] = useState({
         hours: 0,
@@ -41,10 +40,10 @@ const Auction = () => {
                         <TimeBar>
                             <div className="progress">
                                 <span className="time" style={{left: percentage * 0.8 + "%"}}>
-                                    {`${numberWithLength(curTime.hours, 2)} : ${numberWithLength(curTime.minutes, 2)} : ${numberWithLength(curTime.seconds, 2)}`}
+                                    {numberWithLength(curTime.hours, 2)} : {numberWithLength(curTime.minutes, 2)} : {numberWithLength(curTime.seconds, 2)}
                                 </span>
                                 <span className="pointer" style={{left: percentage * 0.8 + 9 + "%"}}></span>
-                                <div class="progress-bar"
+                                <div className="progress-bar"
                                     style={{
                                         width: percentage + "%",
                                         background:
@@ -55,7 +54,7 @@ const Auction = () => {
                             </div>
                         </TimeBar>
                         <TableContainer>
-                            <table class="table">
+                            <table className="table">
                                 <tbody>
                                     <tr>
                                         <td className="item">Token Left</td>
@@ -70,78 +69,40 @@ const Auction = () => {
                         </TableContainer>                        
                     </AuctionLeft>
                     <AuctionRight className="col-md-7">
-
+                        <Title>Exclusive pre round</Title>
+                        <div className="tokenDiv">
+                            <Title><span className="txt-green">100 USD</span> per token</Title>
+                            <Title>amount of Token</Title>
+                            <SliderContainer>
+                                <input
+                                    type="number"
+                                    value={amount}
+                                    onChange={(e) => setAmount(e.target.value)}
+                                    className="range-input"
+                                    min={0}
+                                    max={10}
+                                />
+                                <Slider
+                                    value={amount}
+                                    onChange={(value) => setAmount(value)}
+                                    min={0}
+                                    max={10}
+                                    step={0.1}
+                                />
+                            </SliderContainer>
+                            <TotalPrice>
+                                <Title className="totalPrice">total price</Title>
+                                <div className="price">{price}</div>
+                            </TotalPrice>   
+                            <BuyButton className="w-100">
+                                BUY
+                            </BuyButton>                        
+                        </div>
                     </AuctionRight>
                 </div>
             </section>
         </main>
-    )
-}
+    );
+};
 
-export default Auction
-
-const AuctionLeft = styled.div`
-    height: 90vh;
-    border-right: 2px solid #464646;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    @media screen and (max-width: 768px) {
-        border: none;
-    }
-`;
-
-const AuctionRight = styled.div`
-    height: 90vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
-
-const Title = styled.p`
-    font-size: 30px;
-    font-weight: 700;
-    text-transform: uppercase;
-    @media screen and (max-width: 1200px) {
-        font-size: 24px;
-    }
-    @media screen and (max-width: 768px) {
-        font-size: 20px;
-    }
-`;
-
-const TimeBar = styled.div` 
-    width: 100%;
-    margin-top: 70px;
-    margin-bottom: 50px;
-    position: relative;
-    &>div.progress {
-        height: 12px;
-        border-radius: 0;
-        margin: 0 10%;
-        overflow: visible;
-    }
-    & span.time {
-        position: absolute;
-        font-weight: 700;
-        font-size: 18px;
-        top: -50px;
-        @media screen and (max-width: 768px) {
-            font-size: 12px;
-            top: -40px;
-        }
-    }
-    & span.pointer {
-        position: absolute;
-        height: 25px;        
-        top: -13px;
-        border: 2px solid #de4934;
-    }
-`;
-
-const TableContainer = styled.div`
-    width: 100%;
-    padding: 0 10%;
-`;
+export default Auction;
