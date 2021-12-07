a13dc4bd8fe4fee40fc8ac49200e25ca61e5627c
Mohammad Eskini <eskini@ndb.technology>
2021-12-07 20:06:06 +0330

    Home / Changed the background colors and fixed the grid system on desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,10 @@ import { numberWithCommas } from "../utilities/number"
 
 const IndexPage = () => {
     return (
-        <>
+        <div
+            style={{
+                background: "radial-gradient(54.07% 44.7% at 47.35% 51.98%, rgba(0, 0, 0, 0) 0%, #000000 100%)",
+            }}>
             <Seo title="Home" />
             <main className="home-page">
                 <Header />
@@ -39,7 +42,7 @@ const IndexPage = () => {
                     </div>
                 </section>
             </main>
-        </>
+        </div>
     )
 }
 
