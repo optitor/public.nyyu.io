e336b04548c9cd95a9429b7af67fee2d257d70ba
Julius0505 <se7en.mini.123@protonmail.com>
2021-12-08 04:03:18 +0300

    change backend url to http://auction.us-east-1.elasticbeanstalk.com

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
     // uri: `https://ndb-auction.herokuapp.com/graphql`,
-    uri: `http://localhost:5000/graphql`,
+    // uri: `http://localhost:5000/graphql`,
+    uri: `http://auction.us-east-1.elasticbeanstalk.com/graphql`,
     fetch: fetch,
     fetchOptions: {
         "Access-Control-Allow-Origin": "*",
