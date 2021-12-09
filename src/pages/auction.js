f49f477338c5bd8e04a067081da074d3f7809926
sapronov1021 <sapronov1021@gmail.com>
2021-12-09 20:12:18 +0200

    update auction data

--- src/pages/auction.js
+++ src/pages/auction.js
@@ -55,14 +55,14 @@ const chartData = {
     grid: { top: 8, right: 8, bottom: 24, left: 36 },
     xAxis: {
         type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        data: [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
     },
     yAxis: {
         type: "value",
     },
     series: [
         {
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            data: [820, 932, 901, 934, 1290, 1330, 1320, 300, 467, 1290, 900],
             type: "bar",
             smooth: true,
         },
