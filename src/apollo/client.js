989611f44840011f89f4edf75ab7c6608a48087e
Julius0505 <se7en.mini.123@protonmail.com>
2021-12-19 19:27:16 +0300

    change subscription_base_url to staticData

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -11,7 +11,7 @@ const httpLink = createHttpLink({
 });
 
 const wsLink = new WebSocketLink({
-    uri: 'ws://localhost:5000/subscriptions',
+    uri: SUBSCRIPTION_BASE_URL,
     options: {
         reconnect: true,
         connectionParams: {
