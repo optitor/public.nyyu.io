71a19b0ad8125366de6ed739652f99a30d2e84f4
PavelSemyonov <pavel@ndb.technology>
2022-01-08 04:15:25 +0200

    create avatar dress-up complete but not responsive yet

--- src/apollo/model/avatarComponent.js
+++ src/apollo/model/avatarComponent.js
@@ -1,42 +1,30 @@
-import { useMutation } from "@apollo/client";
-import * as GraphQL from '../graghqls/mutations/AvatarComponent';
-// $groupId: String!
-//         $tierLevel: Int!
-//         $price: Float!
-//         $limited: Int!
-//         $svg: String!
-//         $width: Int!
-//         $top: Int!
-//         $left: Int!
-export const useCreateNewComponentMutation = () => {
-    const [mutation, mutationResults] = useMutation(GraphQL.CREATE_NEW_COMPONENT, {
-        retry: 1,
-        onCompleted: (data) => {
-            if (data.signin.status === "Failed") {
-                // do something
-                return
-            } else if (data.signin.status === "Success") {
-                setUser({
-                    ...getUser(),
-                    tempToken: data.signin.token
-                })
-                navigate("/app/onetime-pwd")
-            }
-        },
-    })
+import { useMutation, useQuery } from "@apollo/client";
+import * as Mutation from '../graghqls/mutations/AvatarComponent';
+import * as Query from './../graghqls/querys/AvatarComponent';
 
-    const signin = (email, password) => {
-        setUser({
-            ...getUser(),
-            tempToken: null,
-            email: email,
-        })
+export const useCreateNewComponentMutation = () => {
+    const [mutation, mutationResults] = useMutation(Mutation.CREATE_NEW_COMPONENT);
+
+    const createNewComponent = (groupId, tierLevel, price, limited, svg, width, top, left) => {
         return mutation({
             variables: {
-                email,
-                password,
+                groupId,
+                tierLevel,
+                price,
+                limited,
+                svg,
+                width,
+                top,
+                left
             },
         })
-    }
-    return [signin, mutationResults]
+    };
+
+    return [createNewComponent, mutationResults];
+};
+
+export const useGetAvatarComponentsQuery = () => {
+    const queryResults = useQuery(Query.GET_AVATAR_COMPONENTS);
+
+    return queryResults;
 };
