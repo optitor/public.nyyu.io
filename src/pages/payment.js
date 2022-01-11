71d1fa29de93ae7929f7f8cc5f815c0e9e6ecb18
PavelSemyonov <pavel@ndb.technology>
2022-01-11 13:31:58 +0200

    create avatar section

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -408,7 +408,7 @@ const Payment = () => {
                                 </div>
                             </TabPanel>
                             <TabPanel className="externalwallets-tab">
-                                <div className="payment-content">
+                                <div className="payment-content" style={{display: 'block'}}>
                                     <ConnectWalletTab />
 
                                     <div className="mt-1 d-flex">
