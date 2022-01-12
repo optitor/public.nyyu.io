eec7239f71de26a74b5359963a0e6e54c89df804
PavelSemyonov <pavel@ndb.technology>
2022-01-12 22:32:10 +0200

    create-user, add-token complete

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,15 +1,5 @@
 import { LOGIN_SUCCESS, LOGOUT_USER } from "../actionTypes";
 
-export const logInUser = authInfo => dispatch => {
-    dispatch({
-        type: LOGIN_SUCCESS,
-        payload: {
-            isAuthenticated: true,
-            user: authInfo
-        }
-    });
-};
-
 export const logOutUser = () => dispatch => {
     dispatch({
         type: LOGOUT_USER
