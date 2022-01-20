c9b8b07f22bbd09d91af6265a0ff1f12f2cfe98f
Julius0505 <se7en.mini.123@protonmail.com>
2022-01-21 00:35:58 +0300

    add avatar component in header and profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ import Header from "../components/header"
 import { ROUTES } from "../utilities/routes"
 import SignOutTab from "./profile/sign-out-tab"
 import { profile_tabs } from "../utilities/staticData"
-import { Tesla, Bronze } from "../utilities/imgImport"
+import { Bronze } from "../utilities/imgImport"
 import TwoFactorModal from "./profile/two-factor-modal"
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
 import { GET_USER } from "../apollo/graghqls/querys/Auth"
@@ -19,7 +19,7 @@ import NotificationRecent from "./profile/notification-recent-switch"
 import NotificationSetting from "./profile/notification-setting-switch"
 import ProfileChangePasswordModal from "./profile/change-password-modal"
 import TierDetailsTab from "./profile/tier-details-tab"
-
+import Avatar from "../components/dress-up/avatar"
 const Profile = () => {
     // Containers
     const { data: userData, refetch } = useQuery(GET_USER, {
@@ -79,7 +79,7 @@ const Profile = () => {
                     <div className="row mt-lg-2">
                         <div className="col-lg-3 profile-page__left border-end border-white">
                             <div className="user-info">
-                                <img className="user-info__avatar" src={Tesla} alt="tesla" />
+                                <Avatar />
                                 <p className="user-info__name">
                                     <img src={Bronze} alt="bronze" className="me-3" />
                                     {displayName}
