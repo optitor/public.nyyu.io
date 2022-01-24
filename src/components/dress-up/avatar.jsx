8250943e98d5705def6ef92be0d6e22452ec56e6
Mohammad Eskini <eskini@ndb.technology>
2022-01-24 17:23:22 +0330

    Select Figure / Items / Fixed the design and using the api data now

--- src/components/dress-up/avatar.jsx
+++ src/components/dress-up/avatar.jsx
@@ -6,7 +6,6 @@ export default function Avatar() {
     const selected = useSelector((state) => state.auth?.user?.avatar?.selected)
 
     const avatar = JSON.parse(selected ?? "[]")
-    // console.log("avatarset", avatar)
 
     return <AvatarImage avatar={{ avatarSet: avatar }} />
 }
