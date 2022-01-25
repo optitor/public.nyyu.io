00024dbc758973d9b0d065db40cc7dad12e4b8a5
Mohammad Eskini <eskini@ndb.technology>
2022-01-25 19:43:51 +0330

    KYC / Step 7 / Fixed the template

--- src/components/auth/verify-id.jsx
+++ src/components/auth/verify-id.jsx
@@ -1,9 +1,5 @@
-import React, { useReducer, useCallback, useRef } from "react"
+import React, { useReducer, useRef } from "react"
 import useFileUpload from "react-use-file-upload"
-import Modal from "react-modal"
-import Webcam from "react-webcam"
-import { formatBytes } from "../../utilities/number"
-import { PhotoIcon, QRCode, SelfieImg, CloseIcon } from "../../utilities/imgImport"
 import SimpleHeader from "../header/simple-header"
 import PrimaryStep from "../verify-identity/primary-step"
 import StepOne from "../verify-identity/step-one"
@@ -59,56 +55,14 @@ const VerificationPage = () => {
     // 6
     const [selfieImage, setSelfieImage] = useState()
 
-    const inputRef = useRef()
-    const webcamRef = useRef(null)
-
     const [state, setState] = useReducer((old, action) => ({ ...old, ...action }), {
-        step: 4, // --> initial value: -1;
-        phoneModal: false,
-        file: null,
-        fileOpen: false,
-        selfieModal: false,
-        selfieImg: "",
+        step: -1, // --> initial value: -1;
     })
-    const { step, phoneModal, file, fileOpen, selfieModal, selfieImg } = state
+    const { step } = state
 
-    const capture = useCallback(() => {
-        setState({ selfieImg: webcamRef.current.getScreenshot() })
-    }, [webcamRef])
+    // Methods
+    const submitKYCData = () => {}
 
-    console.log(selfieImg)
-
-    const FileList = ({ data }) => {
-        return (
-            <li className="file-item">
-                <div className="file-item__info">
-                    <div
-                        onClick={() => {
-                            setState({ fileOpen: true })
-                            setState({ file: data })
-                        }}
-                        onKeyDown={() => setState({ fileOpen: true })}
-                        role="presentation"
-                    >
-                        <img className="mb-2" src={PhotoIcon} alt="file img" />
-                        <div>
-                            <p className="file-name">{data.name}</p>
-                            <p className="file-size">{formatBytes(data.size)}</p>
-                        </div>
-                    </div>
-                    <p
-                        className="remove-file"
-                        // onClick={() => removeFile(data.name)}
-                        // onKeyDown={() => removeFile(data.name)}
-                        role="presentation"
-                    >
-                        <span></span>
-                    </p>
-                </div>
-                <p className="file-item__name">{data.name}</p>
-            </li>
-        )
-    }
     return (
         <main className="verify-page">
             <SimpleHeader />
@@ -200,221 +154,8 @@ const VerificationPage = () => {
                         />
                     )}
                     {step === 6 && <StepSeven step={step} setState={setState} />}
-
-                    {/* <div className="verify-step2">
-                        <h5 className="text-center">Identity document</h5>
-                        <div className="d-flex flex-wrap justify-content-center my-0 my-xxl-5">
-                            <div className="upload-doc me-lg-5">
-                                <div className="mb-3">
-                                    <div
-                                        className={`file-upload ${files.length > 0 && "uploaded"}`}
-                                        onDragEnter={handleDragDropEvent}
-                                        onDragOver={handleDragDropEvent}
-                                        onDrop={(e) => {
-                                            handleDragDropEvent(e)
-                                            setFiles(e, "a")
-                                        }}
-                                        role="presentation"
-                                    >
-                                        <div className="new-doc">
-                                            <img src={NewDoc} alt="new doc" />
-                                        </div>
-                                        <p className="file-browse">
-                                            Drag & drop files here or{" "}
-                                            <span
-                                                onClick={() => inputRef.current.click()}
-                                                onKeyDown={() => inputRef.current.click()}
-                                                role="presentation"
-                                            >
-                                                browse
-                                            </span>
-                                        </p>
-
-                                        <input
-                                            ref={inputRef}
-                                            type="file"
-                                            multiple
-                                            style={{ display: "none" }}
-                                            onChange={(e) => setFiles(e, "a")}
-                                        />
-                                    </div>
-                                </div>
-                                <ul className="file-list">
-                                    {files?.map((item, idx) => (
-                                        <FileList key={idx} data={item} />
-                                    ))}
-                                </ul>
-                            </div>
-                            {files.length > 0 && (
-                                <div className="uploaded-list">
-                                    <p className="uploaded-list__text">You uploaded:</p>
-                                    <ul className="file-list">
-                                        {files?.map((item, idx) => (
-                                            <FileList key={idx} data={item} />
-                                        ))}
-                                    </ul>
-
-                                    <button
-                                        className="btn-add"
-                                        onClick={() => inputRef.current.click()}
-                                    >
-                                        <span></span>Add more files
-                                    </button>
-                                </div>
-                            )}
-                            <div className="upload-rule">
-                                <p>Take a photo of your document. </p>
-                                <p>The photo should be:</p>
-                                <ul>
-                                    <li>
-                                        <strong>bright and clear</strong> (good quality);
-                                    </li>
-                                    <li>
-                                        <strong>uncut</strong> (all corners of the document should
-                                        be visible).
-                                    </li>
-                                </ul>
-                                <div className="upload-rule__img">
-                                    <img src={Pass} alt="pass" />
-                                    <img className="mx-3" src={Unpass1} alt="pass" />
-                                    <img src={Unpass2} alt="pass" />
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
-
-                    {/* {step === 6 && (
-                        <div className="verify-step4">
-                            <div className="selfie-modal__body">
-                                <div className="selfie-content">
-                                    <Webcam
-                                        audio={false}
-                                        ref={webcamRef}
-                                        screenshotFormat="image/jpeg"
-                                        style={{
-                                            width: "100%",
-                                            height: "100%",
-                                        }}
-                                    />
-                                </div>
-                                <div className="btn-group">
-                                    <button
-                                        className="btn-primary me-3"
-                                        onClick={() => setState({ selfieImg: "" })}
-                                    >
-                                        Retake
-                                    </button>
-                                    <button className="btn-primary btn-green" onClick={capture}>
-                                        Upload
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    )} */}
                 </div>
             </section>
-            <Modal
-                isOpen={phoneModal}
-                onRequestClose={() => setState({ phoneModal: false })}
-                ariaHideApp={false}
-                className="phone-modal"
-                overlayClassName="phone-modal__overlay"
-            >
-                <div className="phone-modal__header">
-                    <div
-                        onClick={() => setState({ phoneModal: false })}
-                        onKeyDown={() => setState({ phoneModal: false })}
-                        role="button"
-                        tabIndex="0"
-                    >
-                        <img width="14px" height="14px" src={CloseIcon} alt="close" />
-                    </div>
-                </div>
-                <h4 className="mt-4">Continue verification on your phone</h4>
-                <p className="my-5">Copy a link to your mobile phone</p>
-                <button className="btn-green">Copy Link</button>
-                <p className="my-5">Or scan the QR code with your phone</p>
-                <img src={QRCode} alt="qr code" />
-            </Modal>
-            <Modal
-                isOpen={fileOpen}
-                onRequestClose={() => setState({ fileOpen: false })}
-                ariaHideApp={false}
-                className="file-modal"
-                overlayClassName="file-modal__overlay"
-            >
-                <div className="phone-modal__header">
-                    <div
-                        onClick={() => setState({ fileOpen: false })}
-                        onKeyDown={() => setState({ fileOpen: false })}
-                        role="button"
-                        tabIndex="0"
-                    >
-                        <img width="14px" height="14px" src={CloseIcon} alt="close" />
-                    </div>
-                </div>
-                {file && (
-                    <div className="file-modal__body">
-                        <img src={URL.createObjectURL(file)} alt="file" />
-                        <p>{file.name}</p>
-                    </div>
-                )}
-                <button
-                    className="btn-primary"
-                    onClick={() => {
-                        // removeFile(file.name)
-                        setState({ fileOpen: false })
-                    }}
-                    onKeyDown={() => {
-                        // removeFile(file.name)
-                        setState({ fileOpen: false })
-                    }}
-                >
-                    Delete
-                </button>
-            </Modal>
-            <Modal
-                isOpen={selfieModal}
-                onRequestClose={() => setState({ selfieModal: false })}
-                ariaHideApp={false}
-                className="selfie-modal"
-                overlayClassName="phone-modal__overlay"
-            >
-                <div className="phone-modal__header">
-                    <div
-                        onClick={() => setState({ selfieModal: false })}
-                        onKeyDown={() => setState({ selfieModal: false })}
-                        role="button"
-                        tabIndex="0"
-                    >
-                        <img width="14px" height="14px" src={CloseIcon} alt="close" />
-                    </div>
-                </div>
-                <div className="selfie-modal__body">
-                    <div className="selfie-content">
-                        <Webcam
-                            audio={false}
-                            ref={webcamRef}
-                            screenshotFormat="image/jpeg"
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                            }}
-                        />
-                    </div>
-                    <div className="btn-group">
-                        <button
-                            className="btn-primary me-3"
-                            onClick={() => setState({ selfieImg: "" })}
-                        >
-                            Retake
-                        </button>
-                        <button className="btn-primary btn-green" onClick={capture}>
-                            Upload
-                        </button>
-                    </div>
-                </div>
-            </Modal>
         </main>
     )
 }
