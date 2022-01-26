5560aba135a4fc013e9470d77679d838e19c354b
Mohammad Eskini <eskini@ndb.technology>
2022-01-26 20:22:48 +0330

    KYC / Lazy Loading / Fixed the images jumping on page load

--- src/components/verify-identity/step-seven.jsx
+++ src/components/verify-identity/step-seven.jsx
@@ -7,24 +7,20 @@ import Loading from "../common/Loading"
 export default function StepSeven() {
     // Render
     const [loading, setLoading] = useState(true)
-    if (loading) {
-        return (
-            <>
+    return (
+        <>
+            <div className={`${!loading && "d-none"}`}>
                 <Loading />
-                <img
-                    className="d-none"
-                    src={VerifyIdStep7}
-                    alt="step indicator"
-                    onLoad={() => setLoading(false)}
-                />
-            </>
-        )
-    } else
-        return (
-            <div className="col-sm-12 col-11 mx-auto mt-3 mt-sm-0">
+            </div>
+            <div className={`col-sm-12 col-11 mx-auto mt-3 mt-sm-0 ${loading && "d-none"}`}>
                 <h4 className="text-center  mt-5 mt-sm-2 mb-4">Verify your identity</h4>
                 <div className="text-center">
-                    <img className="d-sm-block d-none" src={VerifyIdStep7} alt="step indicator" />
+                    <img
+                        className="d-sm-block d-none"
+                        src={VerifyIdStep7}
+                        onLoad={() => setLoading(false)}
+                        alt="step indicator"
+                    />
                 </div>
                 <div className="my-sm-5 py-sm-5 verify-step1">
                     <div className="text-sm-start text-center">
@@ -54,5 +50,6 @@ export default function StepSeven() {
                     </div>
                 </div>
             </div>
-        )
+        </>
+    )
 }
