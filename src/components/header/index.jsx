e8eaadf7d8d92ce0ffc2dc53ac79b6e5f8cdf9b5
Mohammad Eskini <eskini@ndb.technology>
2022-01-27 18:14:45 +0330

    Header / Avatar / Added the user tier symbol next to the avatar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -20,6 +20,7 @@ import { setCurrentAuthInfo, getAuthInfo } from "../../redux/actions/authAction"
 import { GET_ALL_UNREAD_NOTIFICATIONS } from "../../apollo/graghqls/querys/Notification"
 import { UPDATE_AVATARSET } from "../../apollo/graghqls/mutations/AvatarComponent"
 import Avatar from "../dress-up/avatar"
+import UserTier from "./user-tier"
 
 const Menu = () => {
     const dispatch = useDispatch()
@@ -193,6 +194,7 @@ const Menu = () => {
                                     <li className="px-sm-3 px-0 scale-75">
                                         <Link to={ROUTES.profile}>
                                             <Avatar className="user-avatar" />
+                                            <UserTier />
                                         </Link>
                                     </li>
                                     <DressupModal
