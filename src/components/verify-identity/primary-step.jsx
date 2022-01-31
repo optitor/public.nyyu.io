c775fa38e1a72ff869a85c3a076124c8412d8b3b
Julius0505 <se7en.mini.123@protonmail.com>
2022-01-31 18:04:13 +0300

    fix language select

--- src/components/verify-identity/primary-step.jsx
+++ src/components/verify-identity/primary-step.jsx
@@ -1,28 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { ROUTES } from "../../utilities/routes"
 import PrivacyPolicy from "./privacy-policy"
-import languageDropdown from "react-lang-dropdown"
 import languages from "../../assets/lang/languages.json"
+import SelectLang from "./select-lang"
 
 export default function PrimaryStep({ step, setState, accept, setAccept }) {
-    const [language, LanguageDropdown] = languageDropdown.useDropdown(languages)
+    const [langKey, setLangKey] = useState("en")
+    const language = languages[langKey]
     return (
         <div className="verify-step0 col-sm-12 col-10 mx-auto mt-5 mt-sm-0">
-            <LanguageDropdown />
-            <h4 className="text-center  mt-5 mt-sm-2 mb-4">{language.content.verify_id}</h4>
+            <SelectLang
+                className="languageDropdown"
+                languages={languages}
+                langKey={langKey}
+                onLangChange={(res) => setLangKey(res.value)}
+            />
+            <h4 className="text-center  mt-5 mt-sm-2 mb-4">{language?.content?.verify_id}</h4>
             <p className="pre-wrap">
                 <span className="text-white fw-bold fs-18px">
-                    {language.content?.subtitle}
+                    {language?.content?.subtitle}
                     <br />
                 </span>
-                {language.content?.content}
+                {language?.content?.content}
             </p>
             <div className="form-group">
                 <PrivacyPolicy
                     agree={accept}
                     setAgree={(res) => setAccept(res)}
-                    lang={language.content}
+                    lang={language?.content}
                 />
             </div>
             <div className="d-flex justify-content-center gap-3 my-5 col-md-12">
