8cc643aaae9e59ebf94048e28f8a34c9fb3dcbe2
Julius0505 <se7en.mini.123@protonmail.com>
2022-01-31 16:02:20 +0300

    fix style of connect mobile

--- src/components/profile/connect-mobile.jsx
+++ src/components/profile/connect-mobile.jsx
@@ -1,52 +1,61 @@
 import React, { useState } from "react"
 import { Input } from "../common/FormControl"
-import { getCountries, getCountryCallingCode } from "react-phone-number-input/input"
-import en from "react-phone-number-input/locale/en.json"
+import Select from "react-select"
+import { countryList } from "../../utilities/countryAlpha2"
+import { getCountryCallingCode } from "react-phone-number-input/input"
 
-const CountrySelect = ({ value, onChange, labels, ...rest }) => (
-    <select {...rest} value={value} onChange={(event) => onChange(event.target.value || undefined)}>
-        <option value="">{labels.ZZ}</option>
-        {getCountries().map((country) => (
-            <option key={country} value={country}>
-                {labels[country]}
-            </option>
-        ))}
-    </select>
-)
+const Countries = countryList.map((item) => {
+    return { label: item.name, value: item["alpha-2"] }
+})
 
 export default function ConnectMobile({ confirm }) {
-    const [country, setCountry] = useState("")
+    const [country, setCountry] = useState(null)
+    const [countryCode, setCountryCode] = useState("")
     const [mobile, setMobile] = useState("")
     return (
-        <div className="input_mobile">
+        <div className="input_mobile form-group">
             <h3>Connect Mobile</h3>
-            <p className="mt-3 pb-3">You will recive a sms code to the number</p>
-            <div className="form-group">
-                <div className="mobile-input-field">
-                    <CountrySelect
-                        className="form-control"
-                        labels={en}
-                        name="countrySelect"
-                        onChange={(c) => {
-                            const code = `+${getCountryCallingCode(c)} `
-                            setCountry(code)
+            <div className="mobile-input-field">
+                <div className="country-code-select">
+                    <svg
+                        className="search-icon text-light"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        ></path>
+                    </svg>
+                    <Select
+                        className="mb-2"
+                        options={Countries}
+                        value={country}
+                        onChange={(selected) => {
+                            const code = `+${getCountryCallingCode(selected?.value)} `
+                            setCountry(selected)
+                            setCountryCode(code)
                             setMobile(code)
                         }}
                     />
-                    <Input
-                        type="text"
-                        value={mobile}
-                        onChange={(e) => {
-                            const input = e.target.value
-                            setMobile(country + input.substr(country.length))
-                        }}
-                    />
                 </div>
-                <p>You will receive a sms code to the number above</p>
-                <button className="btn-primary next-step mt-4" onClick={() => confirm(mobile)}>
-                    Confirm Number
-                </button>
+                <Input
+                    type="text"
+                    value={mobile}
+                    onChange={(e) => {
+                        const input = e.target.value
+                        setMobile(countryCode + input.substr(countryCode.length))
+                    }}
+                />
             </div>
+            <p>You will receive a sms code to the number above</p>
+            <button className="btn-primary next-step mt-4" onClick={() => confirm(mobile)}>
+                Confirm Number
+            </button>
         </div>
     )
 }
