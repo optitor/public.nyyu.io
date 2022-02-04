51df68556e5df10bbe46cbbbbd44175a19a795e9
Mohammad Eskini <eskini@ndb.technology>
2022-02-04 15:39:41 +0330

    Presale / Index Page / Developed the mobile layout

--- src/pages/presale.jsx
+++ src/pages/presale.jsx
@@ -15,7 +15,7 @@ const Presale = () => {
             <Seo title="Direct Purchase" />
             <main className="home-page">
                 <Header />
-                <div className="container col-lg-10 mx-auto gap-5">
+                <div className="container col-lg-10 col-12 mx-auto gap-lg-5 gap-0">
                     <div
                         className="direct-purchase"
                         style={{
@@ -25,8 +25,8 @@ const Presale = () => {
                             backgroundPosition: "left center",
                         }}
                     >
-                        <div className="d-flex align-items-center">
-                            <div className="col-md-6 d-xl-flex flex-xl-column">
+                        <div className="row align-items-center m-0 col-12 px-0">
+                            <div className="col-md-6 d-xl-flex flex-xl-column order-md-0 order-2">
                                 <div className="next-auction-label">
                                     7 days left
                                 </div>
@@ -36,21 +36,26 @@ const Presale = () => {
                                         tokens left
                                     </div>
                                 </div>
+                                <div className="text-center d-block d-md-none mt-5">
+                                    <button className="btn btn-green btn-buy">
+                                        buy
+                                    </button>
+                                </div>
                             </div>
-                            <div className="col-md-6 d-flex flex-column align-items-center">
+                            <div className="col-md-6 d-flex flex-column align-items-center order-md-0 order-1">
                                 <div>
-                                    <div className="exclusive-label">
+                                    <div className="exclusive-label text-center text-md-start">
                                         exlusive pre-sale of
                                     </div>
                                     <div className="ndb-tokens-label">
                                         ndb token
                                     </div>
-                                    <div className="miss-label">
+                                    <div className="miss-label px-5 px-md-0">
                                         Don't miss an opportunity to get ahold
                                         of NDB Tokens before anyone else at the
                                         fixed price before the auction starts.
                                     </div>
-                                    <div className="text-center">
+                                    <div className="text-center d-none d-md-block">
                                         <button className="btn btn-green btn-buy">
                                             buy
                                         </button>
