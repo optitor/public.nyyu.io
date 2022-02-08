fa4e0a17b64b77d42fab0f5740383461de2b18ae
Mohammad Eskini <eskini@ndb.technology>
2022-02-08 13:18:09 +0330

    Wallet / Internal Wallet / the deposit button now looks the same as withdraw btn

--- src/components/wallet/internal-wallet.jsx
+++ src/components/wallet/internal-wallet.jsx
@@ -1,24 +1,24 @@
 /* eslint-disable */
 
 import React, { useEffect, useState, useMemo } from "react"
-import _ from 'lodash';
-import svgToDataURL from 'svg-to-dataurl'
+import _ from "lodash"
+import svgToDataURL from "svg-to-dataurl"
 import axios from "axios"
 import DepositWithdrawModal from "./deposit-withdraw-modal"
-import DepositModal from "./DepositModal";
+import DepositModal from "./DepositModal"
 import CustomSpinner from "../common/custom-spinner"
 import { TRANSACTION_TYPES } from "../../utilities/staticData"
 import NumberFormat from "react-number-format"
 import { useQuery } from "@apollo/client"
 import { GET_BALANCES } from "../../apollo/graghqls/querys/Auth"
-import { Icon } from '@iconify/react';
-import { VOLT, NDB } from './../../utilities/imgImport'
+import { Icon } from "@iconify/react"
+import { VOLT, NDB } from "./../../utilities/imgImport"
 
 const QUOTE = "USDT"
 const TICKER_24hr = "https://api.binance.com/api/v3/ticker/24hr"
-const REFRESH_TIME = 30;
+const REFRESH_TIME = 30
 
-const Asset = ({item}) => {
+const Asset = ({ item }) => {
     return (
         <tr>
             <td className="d-flex align-items-center ps-2">
@@ -29,94 +29,100 @@ const Asset = ({item}) => {
             </td>
             <td>
                 <p className="coin-price fw-bold">
-                    {(item.free + item.hold)} {item.tokenSymbol}
+                    {item.free + item.hold} {item.tokenSymbol}
                 </p>
                 <NumberFormat
-                    value={(item.balance)?.toFixed(2)}
+                    value={item.balance?.toFixed(2)}
                     className="coin-percent"
-                    displayType={'text'}
+                    displayType={"text"}
                     thousandSeparator={true}
                     renderText={(value, props) => <p {...props}>{value} USD</p>}
                 />
             </td>
         </tr>
-    );
+    )
 }
 
 export default function InternalWallet() {
     const InitialMyAssets = {
-        NDB: {tokenSymbol: 'NDB', tokenName: 'NDB', free: 0, hold: 0, symbol: NDB},
-        VOLT: {tokenSymbol: 'VOLT', tokenName: 'Volt token', free: 0, hold: 0, symbol: VOLT},
-    };
-    const [myAssets, setMyAssets] = useState(InitialMyAssets);
-    const [BTCPrice, setBTCPrice] = useState(10000);
+        NDB: { tokenSymbol: "NDB", tokenName: "NDB", free: 0, hold: 0, symbol: NDB },
+        VOLT: { tokenSymbol: "VOLT", tokenName: "Volt token", free: 0, hold: 0, symbol: VOLT },
+    }
+    const [myAssets, setMyAssets] = useState(InitialMyAssets)
+    const [BTCPrice, setBTCPrice] = useState(10000)
 
     const [hideValues, setHideValues] = useState(false)
     const [transactionType, setTransactionType] = useState(TRANSACTION_TYPES.deposit)
     const [showDepositAndWidthdrawModal, setShowDepositAndWidthdrawModal] = useState(false)
-    const [isDepositOpen, setIsDepositOpen] = useState(false);
+    const [isDepositOpen, setIsDepositOpen] = useState(false)
     const obscureValueString = "******"
     const [btcOrUsd, setBtcOrUsd] = useState("USD")
-    
+
     // console.log(myAssets)
     const totalBalance = useMemo(() => {
-        if(!Object.values(myAssets)) return 0;
-        return _.sumBy(Object.values(myAssets), 'balance');
-    }, [myAssets]);
-    
+        if (!Object.values(myAssets)) return 0
+        return _.sumBy(Object.values(myAssets), "balance")
+    }, [myAssets])
+
     useEffect(() => {
         const get_BTCPrice = () => {
-            axios.get(TICKER_24hr, { params: { symbol: 'BTC' + QUOTE } }).then(res => {
-                setBTCPrice(res.data.lastPrice);
-            });
-        };
-        get_BTCPrice();
+            axios.get(TICKER_24hr, { params: { symbol: "BTC" + QUOTE } }).then((res) => {
+                setBTCPrice(res.data.lastPrice)
+            })
+        }
+        get_BTCPrice()
         setInterval(() => {
-            get_BTCPrice();
-        }, 1000 * REFRESH_TIME);
-    }, []);
+            get_BTCPrice()
+        }, 1000 * REFRESH_TIME)
+    }, [])
 
     const { data: balances } = useQuery(GET_BALANCES, {
         fetchPolicy: "network-only",
         onCompleted: () => {
-            if(balances.getBalances) {
-                let assets = balances.getBalances?.map(item => {
-                    return { ...item, symbol: svgToDataURL(item.symbol) };
-                });
-                assets = _.mapKeys(assets, 'tokenSymbol');
-                setMyAssets({...myAssets, ...assets});
+            if (balances.getBalances) {
+                let assets = balances.getBalances?.map((item) => {
+                    return { ...item, symbol: svgToDataURL(item.symbol) }
+                })
+                assets = _.mapKeys(assets, "tokenSymbol")
+                setMyAssets({ ...myAssets, ...assets })
             }
         },
     })
 
     useEffect(() => {
-        (async function() {
+        ;(async function () {
             const get_Balances_Price = async () => {
-                const assets = { ...myAssets };                
-                if(_.isEqual(myAssets, InitialMyAssets)) return;
-                
-                for(const item of Object.values(myAssets)) {
-                    let price = 0;
-                    if(!item.tokenSymbol || item.tokenSymbol === 'NDB' || item.tokenSymbol === 'VOLT') {
-                        price = 0;
+                const assets = { ...myAssets }
+                if (_.isEqual(myAssets, InitialMyAssets)) return
+
+                for (const item of Object.values(myAssets)) {
+                    let price = 0
+                    if (
+                        !item.tokenSymbol ||
+                        item.tokenSymbol === "NDB" ||
+                        item.tokenSymbol === "VOLT"
+                    ) {
+                        price = 0
                     } else {
-                        const res = await axios.get(TICKER_24hr, { params: { symbol: item.tokenSymbol + QUOTE } });
-                        price = res.data.lastPrice;
+                        const res = await axios.get(TICKER_24hr, {
+                            params: { symbol: item.tokenSymbol + QUOTE },
+                        })
+                        price = res.data.lastPrice
                     }
-                    const balance = (item.hold + item.free) * price;
-                    assets[item.tokenSymbol] = { ...item, price, balance };
+                    const balance = (item.hold + item.free) * price
+                    assets[item.tokenSymbol] = { ...item, price, balance }
                 }
-                setMyAssets({...assets});
-            };
+                setMyAssets({ ...assets })
+            }
 
-            get_Balances_Price();
+            get_Balances_Price()
             const interval1 = setInterval(() => {
-                get_Balances_Price();
-            }, 1000 * REFRESH_TIME);
+                get_Balances_Price()
+            }, 1000 * REFRESH_TIME)
 
-            return () => clearInterval(interval1);
-        })();
-    }, [Object.keys(myAssets).length]);
+            return () => clearInterval(interval1)
+        })()
+    }, [Object.keys(myAssets).length])
 
     const loadingSection = !myAssets
 
@@ -135,13 +141,15 @@ export default function InternalWallet() {
                             <div className="d-flex align-items-center">
                                 Equity Value (BTC)
                                 {!hideValues && (
-                                    <Icon className="value-label-eye-icon" 
+                                    <Icon
+                                        className="value-label-eye-icon"
                                         icon="bi:eye-slash"
                                         onClick={() => setHideValues(true)}
                                     />
                                 )}
                                 {hideValues && (
-                                    <Icon className="value-label-eye-icon" 
+                                    <Icon
+                                        className="value-label-eye-icon"
                                         icon="bi:eye"
                                         onClick={() => setHideValues(false)}
                                     />
@@ -172,40 +180,48 @@ export default function InternalWallet() {
                                 </div>
                             </div>
                         </div>
-                        {hideValues?
+                        {hideValues ? (
                             <>
                                 <p className="value">{obscureValueString}</p>
                                 <p className="max-value mt-3">{obscureValueString}</p>
-                            </>:
+                            </>
+                        ) : (
                             <>
                                 <NumberFormat
-                                    value={btcOrUsd === "USD"
-                                        ? totalBalance?.toFixed(2)
-                                        : (totalBalance / BTCPrice).toFixed(9)}
+                                    value={
+                                        btcOrUsd === "USD"
+                                            ? totalBalance?.toFixed(2)
+                                            : (totalBalance / BTCPrice).toFixed(9)
+                                    }
                                     className="value"
-                                    displayType='text'
+                                    displayType="text"
                                     thousandSeparator={true}
                                     renderText={(value, props) => <p {...props}>{value}</p>}
                                 />
                                 <NumberFormat
-                                    value={btcOrUsd === "USD"
-                                        ? (totalBalance / BTCPrice).toFixed(9)
-                                        : totalBalance?.toFixed(2)}
+                                    value={
+                                        btcOrUsd === "USD"
+                                            ? (totalBalance / BTCPrice).toFixed(9)
+                                            : totalBalance?.toFixed(2)
+                                    }
                                     className="max-value mt-3"
-                                    displayType='text'
+                                    displayType="text"
                                     thousandSeparator={true}
-                                    renderText={(value, props) => <p {...props}>~ {value} {btcOrUsd === 'USD'? 'BTC': 'USD'}</p>}
+                                    renderText={(value, props) => (
+                                        <p {...props}>
+                                            ~ {value} {btcOrUsd === "USD" ? "BTC" : "USD"}
+                                        </p>
+                                    )}
                                 />
                             </>
-                        }
+                        )}
                     </div>
                     <div className="btn-group d-flex justify-content-between mt-3 align-items-center">
                         <div className="col-sm-6 pe-2">
                             <button
-                                // disabled
-                                className="btn btn-outline-secondary rounded-0 col-12 text-uppercase fw-bold py-2 h4"
+                                className="btn btn-outline-light rounded-0 col-12 text-uppercase fw-bold py-2 h4"
                                 onClick={() => {
-                                    setIsDepositOpen(true);
+                                    setIsDepositOpen(true)
                                 }}
                             >
                                 deposit
@@ -241,13 +257,15 @@ export default function InternalWallet() {
                 <div>
                     <table className="my-3">
                         <tbody>
-                            {_.map((_.orderBy(myAssets, ['balance'], ['desc'])), (item) => <Asset item={item} key={item.tokenName} />)}
+                            {_.map(_.orderBy(myAssets, ["balance"], ["desc"]), (item) => (
+                                <Asset item={item} key={item.tokenName} />
+                            ))}
                             {Object.values(myAssets).length === 0 && (
                                 <p className="text-center fw-500 text-uppercase">no assets found</p>
                             )}
                         </tbody>
                     </table>
                 </div>
-            </div>            
+            </div>
         )
 }
