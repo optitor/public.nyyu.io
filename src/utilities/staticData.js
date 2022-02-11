4fe78fc2fcfdaba6d49ee661e561155768794d70
Mohammad Eskini <eskini@ndb.technology>
2022-02-11 15:28:44 +0330

    KYC / Address Verification / Removed other types of documents, we only support national_id

--- src/utilities/staticData.js
+++ src/utilities/staticData.js
@@ -1450,34 +1450,34 @@ export const VerificationDocumentTypes = [
 export const VerificationStepThreeDocumentTypes = [
     {
         label: "National ID",
-        value: "passport",
+        value: "national_id",
     },
-    {
-        label: "Driving license",
-        value: "driving_license",
-    },
-    {
-        label: "Utility bill",
-        value: "utility_bill",
-    },
-    {
-        label: "Rent agreement",
-        value: "rent_agreement",
-    },
-    {
-        label: "Employer letter",
-        value: "employer_letter",
-    },
-    {
-        label: "Tax bill",
-        value: "tax_bill",
-    },
-    {
-        label: "Bank statement",
-        value: "bank_statement",
-    },
-    {
-        label: "Insurence agreement",
-        value: "insurance_agreement",
-    },
+    // {
+    //     label: "Driving license",
+    //     value: "driving_license",
+    // },
+    // {
+    //     label: "Utility bill",
+    //     value: "utility_bill",
+    // },
+    // {
+    //     label: "Rent agreement",
+    //     value: "rent_agreement",
+    // },
+    // {
+    //     label: "Employer letter",
+    //     value: "employer_letter",
+    // },
+    // {
+    //     label: "Tax bill",
+    //     value: "tax_bill",
+    // },
+    // {
+    //     label: "Bank statement",
+    //     value: "bank_statement",
+    // },
+    // {
+    //     label: "Insurence agreement",
+    //     value: "insurance_agreement",
+    // },
 ]
