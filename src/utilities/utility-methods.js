bfe79ecb6c947a7c9ad055996d9337e069a84489
Mohammad Eskini <eskini@ndb.technology>
2022-02-15 18:27:17 +0330

    KYC / Get Shufti Status / Fixed the issue with 400 code

--- src/utilities/utility-methods.js
+++ src/utilities/utility-methods.js
@@ -13,39 +13,46 @@ export const getBase64 = (file) => {
 export const getShuftiStatusByReference = async (reference) => {
     if (!reference) return "INVALID"
 
-    const { data } = await axios.post(
-        "https://api.shuftipro.com/status",
-        {
-            reference,
-        },
-        {
-            headers: {
-                Authorization: `Basic ${btoa(`${CLIENT_ID}:${SECRET}`)}`,
+    const response = await axios
+        .post(
+            "https://api.shuftipro.com/status",
+            {
+                reference,
             },
-        }
-    )
+            {
+                headers: {
+                    Authorization: `Basic ${btoa(`${CLIENT_ID}:${SECRET}`)}`,
+                },
+            }
+        )
+        .catch((error) => console.log(error))
 
-    if ("verification_result" in data && "event" in data) {
-        const output = {
-            event: data.event,
-            docStatus:
-                data.verification_result.document.document === 1 ||
-                (data.verification_result.document.document_must_not_be_expired === 1 &&
-                    data.verification_result.document.document_visibility === 1 &&
-                    data.verification_result.document.selected_type),
-            addrStatus:
-                data.verification_result.address.full_address === 1 &&
-                data.verification_result.address.match_address_proofs_with_document_proofs === 1 &&
-                data.verification_result.address.address_document_must_not_be_expired === 1 &&
-                data.verification_result.address.selected_type &&
-                data.verification_result.address.address_document_visibility === 1 &&
-                data.verification_result.address.address_document === 1,
-            conStatus:
-                data.verification_result.consent.consent === 1 &&
-                data.verification_result.consent.selected_type,
-            selfieStatus: data.verification_result.face === 1,
+    if (response && "data" in response) {
+        const { data } = response
+        if ("verification_result" in data && "event" in data) {
+            const output = {
+                event: data.event,
+                docStatus:
+                    data.verification_result.document.document === 1 ||
+                    (data.verification_result.document.document_must_not_be_expired === 1 &&
+                        data.verification_result.document.document_visibility === 1 &&
+                        data.verification_result.document.selected_type),
+                addrStatus:
+                    data.verification_result.address.full_address === 1 &&
+                    data.verification_result.address.match_address_proofs_with_document_proofs ===
+                        1 &&
+                    data.verification_result.address.address_document_must_not_be_expired === 1 &&
+                    data.verification_result.address.selected_type &&
+                    data.verification_result.address.address_document_visibility === 1 &&
+                    data.verification_result.address.address_document === 1,
+                conStatus:
+                    data.verification_result.consent.consent === 1 &&
+                    data.verification_result.consent.selected_type,
+                selfieStatus: data.verification_result.face === 1,
+            }
+            return output
         }
-        return output
+        return "INVALID"
     }
 
     return "UNSET"
