6db7fc530eb12ac318fb11c9d0c0ad23c3c436f3
Mohammad Eskini <eskini@ndb.technology>
2022-02-18 19:33:28 +0330

    2FA / Revered the changes

--- src/components/auth/verify-multiFA.jsx
+++ src/components/auth/verify-multiFA.jsx
@@ -51,7 +51,7 @@ const VerifyMutliFA = ({ twoStep, tempToken, email, returnToSignIn }) => {
                         (step) =>
                             step && (
                                 <div key={step}>
-                                    <p className="text-capitalize">code</p>
+                                    <p className="text-capitalize">{step}</p>
                                     <div className="form-group">
                                         <Input
                                             name="code"
