1d233d21034c7c201992946f4194bf84f94f183b
Mohammad Eskini <eskini@ndb.technology>
2022-02-24 20:29:43 +0330

    Auction / Auction Context /Added Presales data

--- src/components/auction/auction-context.jsx
+++ src/components/auction/auction-context.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@apollo/client"
 import React, { useContext } from "react"
 import { useState } from "react"
-import { GET_AUCTION } from "../../apollo/graghqls/querys/Auction"
+import { GET_AUCTION, GET_PRESALES } from "../../apollo/graghqls/querys/Auction"
 
 export const AuctionContext = React.createContext()
 export const useAuction = () => useContext(AuctionContext)
@@ -14,14 +14,26 @@ const AuctionProvider = ({ children }) => {
     const [bidModal, setBidModal] = useState(false)
     const [getBid, setGetBid] = useState(null)
     const [isBid, setIsBid] = useState(null)
+    const [presales, setPresales] = useState(null)
 
-    const loading = !auctions
+    // PreSale
+
+    const loading = !(auctions && presales)
 
     // Webservices
     useQuery(GET_AUCTION, {
         onCompleted: (data) => {
             setAuctions(data.getAuctions)
             setCurrentRoundNumber(data.getAuctions.length)
+        },
+        onError: (error) => console.log(error),
+        errorPolicy: "ignore",
+        fetchPolicy: "network-only",
+    })
+
+    useQuery(GET_PRESALES, {
+        onCompleted: (data) => {
+            setPresales(data.getPreSales)
         },
         onError: (error) => console.log(error),
         errorPolicy: "ignore",
