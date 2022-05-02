5d509004d61e9aa7cbb77a7ed9e1d3c1c4e7937e
pavelsemyonov <pavel@ndb.technology>
2022-05-02 15:52:27 +0300

    Fixed the approval modal in crypto withdraw and paypal withdraw

--- src/apollo/graphqls/querys/Approval.js
+++ src/apollo/graphqls/querys/Approval.js
@@ -91,11 +91,59 @@ export const GET_CRYPTO_WITHDRAW_BY_ID = gql`
     }
 `;
 
+export const GET_CRYPTO_WITHDRAW_BY_ID_BY_ADMIN = gql`
+    query GetCryptoWithdrawByIdByAdmin(
+        $id: Int
+    ) {
+        getCryptoWithdrawByIdByAdmin(
+            id: $id
+        ) {
+            id
+            userId
+            email
+            sourceToken
+            withdrawAmount
+            fee
+            tokenAmount
+            status
+            deniedReason
+            requestedAt
+            confirmedAt
+            network
+            destination
+        }
+    }
+`;
+
 export const GET_PAYPAL_WITHDRAW_BY_ID = gql`
     query GetPaypalWithdrawById(
         $id: Int
     ) {
         getPaypalWithdrawById(
+            id: $id
+        ) {
+            id
+            userId
+            email
+            targetCurrency
+            sourceToken
+            withdrawAmount
+            fee
+            tokenAmount
+            status
+            deniedReason
+            requestedAt
+            confirmedAt
+            receiver
+        }
+    }
+`;
+
+export const GET_PAYPAL_WITHDRAW_BY_ID_BY_ADMIN = gql`
+    query getPaypalWithdrawByIdByAdmin(
+        $id: Int
+    ) {
+        getPaypalWithdrawByIdByAdmin(
             id: $id
         ) {
             id
