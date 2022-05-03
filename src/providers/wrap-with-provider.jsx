93b1327b849ea2e37ff09317865265d35b99ba90
xcellent-dev <xcellentdev@gmail.com>
2022-05-03 15:24:18 +0300

    fix: window is not defined

--- src/providers/wrap-with-provider.jsx
+++ src/providers/wrap-with-provider.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { AuthProvider } from "../hooks/useAuth"
 import { ApolloProvider } from "@apollo/client"
 import { Provider as ReduxProvider } from "react-redux";
-
+import { isBrowser } from "../../utilities/auth";
 import { Provider as WalletProvider, chain, defaultChains, createClient } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
@@ -22,7 +22,9 @@ const chains = defaultChains;
 
 // Set up connectors
 const defaultChain = chain.mainnet;
-window.Buffer = window.Buffer || require("buffer").Buffer;
+if(window !== undefined) {
+  window.Buffer = window.Buffer || require("buffer").Buffer;
+}
 const connectors = createClient({
     autoConnect: true,
     connectors({chainId}) {
