b23587f7d987a8b22e5d19aeaba063324211b7e3
pavelsemyonov <pavel@ndb.technology>
2022-06-09 12:05:58 +0300

    static3

--- src/utilities/staticData3.js
+++ src/utilities/staticData3.js
@@ -1,6 +1,6 @@
 
-export const API_BASE_URL = "https://api.dev.nyyu.io"
-export const SITE_URL = "https://dev.nyyu.io"
+export const API_BASE_URL = "https://api.nyyu.io"
+export const SITE_URL = "https://www.nyyu.io"
 
 export const CurrencyIconEndpoint = 'https://currencyfreaks.com/photos/flags';
 export const ZENDESK_KEY = "18a2747f-28bb-4d78-b0c9-f3bd5047dd31";
