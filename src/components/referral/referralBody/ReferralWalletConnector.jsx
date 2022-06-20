74cb7d1be77333e576599f05ae632c2f525c850c
xcellent-dev <xcellentdev@gmail.com>
2022-06-20 23:15:24 +0800

    fix: update wallet

--- src/components/referral/referralBody/ReferralWalletConnector.jsx
+++ src/components/referral/referralBody/ReferralWalletConnector.jsx
@@ -82,16 +82,30 @@ const ReferralWalletConnector = ({referrerInfo, setReferrer, action}) => {
             setError('Unknown error.')
         }
     }
+
+    const onPrevious = () => {
+        if(selectedWallet === 'external') {
+            // go back to select wallet
+            setSelectedWallet('internal');
+        } else if(action === UPDATE_ACTION) {
+            // go back to link
+            setReferrer({...referrerInfo});
+        }
+    }
     
     return <div className='text-white'>
         <div>
-            {/* <div>STEP <span className='text-green'>2</span></div> */}
-            <div>
-                {selectedWallet === 'external' && 
-                    <><FaArrowLeft onClick={() => setSelectedWallet('internal')}/>&nbsp;</>
-                }
-                Connect your wallet
-            </div>
+            <button 
+                className='bg-transparent border-0' 
+                onClick={onPrevious}
+            >
+                <div className='d-flex align-items-center'>
+                    {(selectedWallet === 'external' || action === UPDATE_ACTION) && 
+                        <><FaArrowLeft />&nbsp;</>
+                    }
+                    <span>Connect your wallet</span>
+                </div>
+            </button>
         </div>
         <div>
             <WalletSelector 
@@ -108,17 +122,9 @@ const ReferralWalletConnector = ({referrerInfo, setReferrer, action}) => {
                 <div className={`${(activateLoading || updateLoading) ? "opacity-1": "opacity-0"}`}><CustomSpinner /></div>
                 <div className={`fs-20px ${(activateLoading || updateLoading) ? 'ms-3' : 'pe-4'}`}>
                     {action === ACTIVE_ACTION && 'ACTIVATE INVITE & EARN'}
-                    {action === UPDATE_ACTION && 'UPDATE REFERRAL WALLET'}
+                    {action === UPDATE_ACTION && 'UPDATE'}
                 </div>
             </button>
-            {action === UPDATE_ACTION && 
-                <button
-                    onClick={() => setReferrer({...referrerInfo})}
-                    className='d-flex align-items-center justify-content-center w-100 py-2 mt-3 bg-transparent text-white fw-bold border border-white fs-22px'
-                >
-                    CANCEL UPDATE
-                </button>
-            }
         </div>
     </div>
 }
