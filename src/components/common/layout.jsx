2aacdd6f2356cdaa9e5cf2f936819a1dfe49b82a
problockchain0101 <problockchain0101@protonmail.com>
2022-10-01 19:37:57 +0700

    fix Cookie consent banner not display .

--- src/components/common/layout.jsx
+++ src/components/common/layout.jsx
@@ -1,11 +1,18 @@
 import * as React from "react"
 import Header from "../header"
+import CookieConsent from 'react-cookie-consent';
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
             <section>{children}</section>
+            <CookieConsent
+                    location="bottom"
+                    buttonText="Accept"
+                    declineButtonText="Decline"
+                    cookieName="gatsby-gdpr-google-analytics">
+            </CookieConsent>
         </>
     )
 }
