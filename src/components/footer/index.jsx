a328559008dca51cc6daf74ee3b0d0573767eb55
problockchain0101 <problockchain0101@protonmail.com>
2022-10-01 21:20:02 +0700

    make correct cookie consent banner text ,  align .

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,28 @@
 import React from "react"
 import { footerLinks } from "../../utilities/staticData"
-
+import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
 const FooterStatement = '© 2022 NYYU UAB. Authorised by the FCIU, Reg. 305951620.';
 
 export default function Footer() {
 
     return (
+        
         <div className="footer-bar">
+            <CookieConsent
+                location="bottom"
+                buttonText="Yes, I agree"
+                cookieName="gatsby-gdpr-google-analytics"
+                style={{ background: "#333333" }}
+                buttonStyle={{ color: "#23C865", background: "#333333", fontSize: "14px" }}
+                declineButtonStyle={{ color: "#FFFFFF", background: "#333333", fontSize: "14px" }}
+                expires={365}
+            >
+                <p style={{ color: "white", fontSize: "14px", textAlign: "left" }}>
+                We use cookies to give you the best online experience. Please let us know if you agree to
+                all of these cookies.
+                <br />
+                </p>
+            </CookieConsent>
             <div className="row">
                 <div className="col-xl-4 d-none d-xl-block text-center">
                     <div className="text-lightgrey py-1">
